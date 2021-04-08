97257a1016d26e7107e5cca3cf268421054f8aa9
erikfazio <fazioerik@gmail.com>
2021-04-08 10:55:42 +0200

    Add design-react-kit UI library

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import "bootstrap-italia/dist/css/bootstrap-italia.min.css";
+import "typeface-titillium-web";
+import "typeface-roboto-mono";
+import "typeface-lora";
 
 function App() {
   return (
