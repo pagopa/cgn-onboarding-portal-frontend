d62023214bf3a7425b3a7519ba290d1791f73b89
Mattia Ripamonti <mattia.ripamonti@bitrock.it>
2021-05-11 14:59:57 +0200

    fix build

--- src/store/user/userSlice.ts
+++ src/store/user/userSlice.ts
@@ -4,7 +4,7 @@ import jwt_decode from 'jwt-decode';
 
 export const userSlice = createSlice({
 	name: 'user',
-	initialState: { data: { level: '' }, type: '', loading: true },
+	initialState: { data: { level: '', company: { organization_name: '', organization_fiscal_code: ''} } , type: '', loading: true },
 	reducers: {
 		setUser: (state, action) => {
 			const decoded = jwt_decode(action.payload) as any;
