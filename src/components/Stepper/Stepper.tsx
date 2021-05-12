a05df95da7a3ff967a457073a0972814dab15270
Mattia Ripamonti <mattia.ripamonti@bitrock.it>
2021-05-12 09:08:29 +0200

    fix handle next step

--- src/components/Stepper/Stepper.tsx
+++ src/components/Stepper/Stepper.tsx
@@ -18,38 +18,45 @@ const Stepper = ({
   completedSteps,
   handleChangeStep,
   steps
-}: Props) => (
-  <div className="steppers bg-white shadow-sm">
-    <div className="container">
-      <ul className="steppers-header">
-        {steps.map((step: Step, index: number) => (
-          <Step
-            key={step.key}
-            index={index + 1}
-            stepType={
-              activeStep === index
-                ? "active"
-                : completedSteps.includes(step.key)
-                ? "confirmed"
-                : ""
-            }
-            handleChangeStep={
-              completedSteps.includes(step.key)
-                ? () => handleChangeStep(index)
-                : null
-            }
-          >
-            {step.label}
-          </Step>
-        ))}
-        <li className="steppers-index" aria-hidden="true">
+}: Props) => {
+  const changeStep = (stepKey: string, index: number) => {
+    if (
+      completedSteps.includes(stepKey) ||
+      completedSteps.includes(steps[index - 1]?.key)
+    ) {
+      return () => handleChangeStep(index);
+    }
+  };
+
+  return (
+    <div className="steppers bg-white shadow-sm">
+      <div className="container">
+        <ul className="steppers-header">
           {steps.map((step: Step, index: number) => (
-            <span key={step.key}>{index}</span>
+            <Step
+              key={step.key}
+              index={index + 1}
+              stepType={
+                activeStep === index
+                  ? "active"
+                  : completedSteps.includes(step.key)
+                  ? "confirmed"
+                  : ""
+              }
+              handleChangeStep={changeStep(step.key, index)}
+            >
+              {step.label}
+            </Step>
           ))}
-        </li>
-      </ul>
+          <li className="steppers-index" aria-hidden="true">
+            {steps.map((step: Step, index: number) => (
+              <span key={step.key}>{index}</span>
+            ))}
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Stepper;
