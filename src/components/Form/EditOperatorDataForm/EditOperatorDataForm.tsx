e03464be187d7cedebdc926d21fab81ef5c014c8
erikfazio <fazioerik@gmail.com>
2021-05-12 16:40:28 +0200

    Fix edit operator data form

--- src/components/Form/EditOperatorDataForm/EditOperatorDataForm.tsx
+++ src/components/Form/EditOperatorDataForm/EditOperatorDataForm.tsx
@@ -2,134 +2,197 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Form, Formik } from "formik";
 import * as Yup from "yup";
-import { Button } from "design-react-kit";
-import { Link, useHistory } from "react-router-dom";
 import { tryCatch } from "fp-ts/lib/TaskEither";
 import { toError } from "fp-ts/lib/Either";
+import CenteredLoading from "../../CenteredLoading/CenteredLoading";
+import FormContainer from "../FormContainer";
+import Api from "../../../api";
+import { RootState } from "../../../store/store";
 import ProfileInfo from "../CreateProfileForm/ProfileData/ProfileInfo";
 import ReferentData from "../CreateProfileForm/ProfileData/ReferentData";
-import Api from "../../../api";
-import { DASHBOARD } from "../../../navigation/routes";
-import { RootState } from "../../../store/store";
+import ProfileImage from "../CreateProfileForm/ProfileData/ProfileImage";
+import ProfileDescription from "../CreateProfileForm/ProfileData/ProfileDescription";
+import SalesChannels from "../CreateProfileForm/ProfileData/SalesChannels";
 
-const EditOperatorDataForm = () => {
-  const history = useHistory();
+const defaultSalesChannel = {
+  channelType: "",
+  websiteUrl: "",
+  discountCodeType: "",
+  addresses: [{ street: "", zipCode: "", city: "", district: "" }]
+};
+
+const defaultInitialValues = {
+  fullName: "",
+  hasDifferentFullName: false,
+  name: "",
+  pecAddress: "",
+  taxCodeOrVat: "",
+  legalOffice: "",
+  telephoneNumber: "",
+  legalRepresentativeFullName: "",
+  legalRepresentativeTaxCode: "",
+  referent: {
+    firstName: "",
+    lastName: "",
+    role: "",
+    emailAddress: "",
+    telephoneNumber: ""
+  },
+  description: "",
+  salesChannel: defaultSalesChannel
+};
+
+const validationSchema = Yup.object().shape({
+  hasDifferentName: Yup.boolean(),
+  name: Yup.string().when(["hasDifferentName"], {
+    is: true,
+    then: Yup.string().required("Campo obbligatorio")
+  }),
+  pecAddress: Yup.string()
+    .email("Deve essere una email")
+    .required("Campo obbligatorio"),
+  legalOffice: Yup.string().required("Campo obbligatorio"),
+  telephoneNumber: Yup.string()
+    .max(15, "Massimo 15 caratteri")
+    .required("Campo obbligatorio"),
+  legalRepresentativeFullName: Yup.string().required(),
+  legalRepresentativeTaxCode: Yup.string()
+    .min(16, "Deve essere di 16 caratteri")
+    .max(16, "Deve essere di 16 caratteri")
+    .required("Campo obbligatorio"),
+  referent: Yup.object().shape({
+    firstName: Yup.string().required("Campo obbligatorio"),
+    lastName: Yup.string().required("Campo obbligatorio"),
+    role: Yup.string().required("Campo obbligatorio"),
+    emailAddress: Yup.string()
+      .email("Deve essere una email")
+      .required("Campo obbligatorio"),
+    telephoneNumber: Yup.string()
+      .max(15, "Massimo 15 caratteri")
+      .required("Campo obbligatorio")
+  }),
+  description: Yup.string().required("Campo obbligatorio"),
+  salesChannel: Yup.object().shape({
+    channelType: Yup.mixed().oneOf([
+      "OnlineChannel",
+      "OfflineChannel",
+      "BothChannels"
+    ]),
+    websiteUrl: Yup.string().when("channelType", {
+      is: "OnlineChannel" || "BothChannels",
+      then: Yup.string().required("Campo obbligatorio")
+    }),
+    discountCodeType: Yup.string().when("channelType", {
+      is: "OnlineChannel" || "BothChannels",
+      then: Yup.string().required("Campo obbligatorio")
+    }),
+    addresses: Yup.array().when("channelType", {
+      is: "OfflineChannel" || "BothChannels",
+      then: Yup.array().of(
+        Yup.object().shape({
+          street: Yup.string().required("Campo obbligatorio"),
+          zipCode: Yup.string().required("Campo obbligatorio"),
+          city: Yup.string().required("Campo obbligatorio"),
+          district: Yup.string().required("Campo obbligatorio")
+        })
+      )
+    })
+  })
+});
+
+type Props = {
+  isCompleted: boolean;
+  handleBack: () => void;
+  handleNext: () => void;
+};
+
+const EditOperatorDataForm = ({
+  isCompleted,
+  handleBack,
+  handleNext
+}: Props) => {
   const agreement = useSelector((state: RootState) => state.agreement.value);
-  const [currentProfile, setCurrentProfile] = useState<any>();
+  const user = useSelector((state: RootState) => state.user.data);
+  const [initialValues, setInitialValues] = useState<any>(defaultInitialValues);
+  const [loading, setLoading] = useState(true);
+
+  const updateProfile = (discount: any) => {
+    if (agreement) {
+      void Api.Profile.updateProfile(agreement.id, discount);
+    }
+  };
+
+  const submitProfile = () => updateProfile;
 
   const getProfile = async (agreementId: string) =>
     await tryCatch(() => Api.Profile.getProfile(agreementId), toError)
       .map(response => response.data)
       .fold(
-        () => void 0,
-        profile => setCurrentProfile(profile)
+        () => setLoading(false),
+        profile => {
+          setInitialValues({
+            ...profile,
+            hasDifferentFullName: !!profile.name
+          });
+          setLoading(false);
+        }
       )
       .run();
 
   useEffect(() => {
+    setLoading(true);
     void getProfile(agreement.id);
   }, []);
 
-  const editProfile = (profile: any) => {
-    if (agreement.id) {
-      void Api.Profile.updateProfile(agreement.id, profile);
-    }
-  };
-
-  const validationSchema = Yup.object().shape({
-    fullName: Yup.string(),
-    hasDifferentName: Yup.boolean(),
-    name: Yup.string().when(["hasDifferentName"], {
-      is: true,
-      then: Yup.string().required()
-    }),
-    pecAddress: Yup.string()
-      .email()
-      .required(),
-    legalOffice: Yup.string().required(),
-    telephoneNumber: Yup.string()
-      .max(15)
-      .required(),
-    legalRepresentativeFullName: Yup.string().required(),
-    legalRepresentativeTaxCode: Yup.string().required(),
-    referent: Yup.object().shape({
-      firstName: Yup.string().required(),
-      lastName: Yup.string().required(),
-      role: Yup.string().required(),
-      emailAddress: Yup.string()
-        .email()
-        .required(),
-      telephoneNumber: Yup.string()
-        .max(15)
-        .required()
-    }),
-    description: Yup.string().required(),
-    salesChannel: Yup.object().shape({
-      channelType: Yup.mixed().oneOf([
-        "OnlineChannel",
-        "OfflineChannel",
-        "BothChannels"
-      ]),
-      websiteUrl: Yup.string().when("channelType", {
-        is: "OnlineChannel" || "BothChannels",
-        then: Yup.string().required()
-      }),
-      discountCodeType: Yup.string().when("channelType", {
-        is: "OnlineChannel" || "BothChannels",
-        then: Yup.string().required()
-      }),
-      addresses: Yup.array().when("channelType", {
-        is: "OfflineChannel" || "BothChannels",
-        then: Yup.array().of(
-          Yup.object().shape({
-            street: Yup.string().required(),
-            zipCode: Yup.string().required(),
-            city: Yup.string().required(),
-            district: Yup.string().required()
-          })
-        )
-      })
-    })
-  });
+  if (loading) {
+    return <CenteredLoading />;
+  }
 
   return (
-    <>
-      {currentProfile && (
-        <Formik
-          initialValues={currentProfile}
-          validationSchema={validationSchema}
-          onSubmit={values => {
-            const { hasDifferentFullName, ...profile } = values;
-            editProfile(profile);
-            history.push(DASHBOARD);
-          }}
-        >
-          {({ values }) => (
-            <Form autoComplete="off">
-              <ProfileInfo formValues={values} />
-              <ReferentData>
-                <div className="mt-10">
-                  <Link
-                    to={DASHBOARD}
-                    className="px-14 mr-4 btn btn-outline-primary"
-                  >
-                    Indietro
-                  </Link>
-                  <Button
-                    className="px-14"
-                    color="primary"
-                    tag="button"
-                    type="submit"
-                  >
-                    Salva
-                  </Button>
-                </div>
-              </ReferentData>
-            </Form>
-          )}
-        </Formik>
+    <Formik
+      initialValues={{
+        ...initialValues,
+        salesChannel: {
+          ...defaultSalesChannel,
+          ...initialValues.salesChannel
+        },
+        fullName: user.company?.organization_name || "",
+        taxCodeOrVat:
+          user.company?.organization_fiscal_code || user.fiscal_number || ""
+      }}
+      validationSchema={validationSchema}
+      onSubmit={values => {
+        const { hasDifferentFullName, ...discount } = values;
+        if (discount.salesChannel?.channelType === "OnlineChannel") {
+          const newSalesChannel = discount.salesChannel;
+          const { addresses, ...salesChannel } = newSalesChannel;
+          submitProfile()({ ...discount, salesChannel });
+        } else {
+          const newSalesChannel = discount.salesChannel;
+          const {
+            websiteUrl,
+            discountCodeType,
+            ...salesChannel
+          } = newSalesChannel;
+          submitProfile()({ ...discount, salesChannel });
+        }
+        handleNext();
+      }}
+    >
+      {({ values, isValid }) => (
+        <Form autoComplete="off">
+          <ProfileInfo formValues={values} />
+          <ReferentData />
+          <ProfileImage />
+          <ProfileDescription />
+          <SalesChannels
+            handleBack={handleBack}
+            formValues={values}
+            isValid={isValid}
+          />
+        </Form>
       )}
-    </>
+    </Formik>
   );
 };
 
