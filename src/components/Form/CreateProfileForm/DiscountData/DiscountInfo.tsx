36a7b76a8b719164a3a8ebb0914d8cc53e0ae189
AleDore <alessio85.dore@gmail.com>
2021-07-27 13:12:19 +0200

    remove required label for discount

--- src/components/Form/CreateProfileForm/DiscountData/DiscountInfo.tsx
+++ src/components/Form/CreateProfileForm/DiscountData/DiscountInfo.tsx
@@ -121,7 +121,6 @@ const DiscountInfo = ({ formValues, setFieldValue, index }: Props) => {
         title="Entità dello sconto"
         description="Inserire la percentuale di sconto erogata"
         isVisible
-        required
       >
         <Field
           id="discount"
