f53b52bf1b909ec9c30a912c492b7626e7771522
CrisTofani <cri.tofani@gmail.com>
2021-12-02 16:53:15 +0100

    [PE-235] Fix Admin logout to clear all cookie and session info

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -1,21 +1,24 @@
 import * as Msal from "@azure/msal-browser";
 
-const msalConfig = {
+const msalConfig: Msal.Configuration = {
   auth: {
     clientId: process.env.MSAL_CLIENT_ID as string,
     authority: process.env.MSAL_AUTHORITY as string,
-    knownAuthorities: ["testcgnportalbitrock.b2clogin.com", "cgnonboardingportaluat.b2clogin.com"], // You must identify your tenant's domain as a known authority.
+    knownAuthorities: [
+      "testcgnportalbitrock.b2clogin.com",
+      "cgnonboardingportaluat.b2clogin.com"
+    ], // You must identify your tenant's domain as a known authority.
     redirectUri: process.env.MSAL_REDIRECT_URI as string,
+    postLogoutRedirectUri: "/"
   },
   cache: {
     cacheLocation: "sessionStorage",
-    storeAuthStateInCookie: false,
+    storeAuthStateInCookie: false
   }
-};  
-  
+};
+
 export const loginRequest = {
   scopes: ["openid", process.env.MSAL_CLIENT_ID as string]
 };
-
 
 export const AdminAccess = new Msal.PublicClientApplication(msalConfig);
