3d392630e4b8069781702d464d9788f874ed4eae
CrisTofani <cri.tofani@gmail.com>
2021-12-22 12:28:54 +0100

    Updates department logo

--- src/components/Logo/DepartmentLogo.tsx
+++ src/components/Logo/DepartmentLogo.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import logoDipartimento from "../../assets/images/logo-dipartimento.png";
 
 const DeparmentLogo = () => (
-  <img src={logoDipartimento} style={{ width: "223px", height: "94px" }} />
+  <img src={logoDipartimento} style={{ width: "450px", height: "80px" }} />
 );
 
 export default DeparmentLogo;
