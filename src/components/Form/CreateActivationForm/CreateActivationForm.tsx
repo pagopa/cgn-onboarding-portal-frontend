d116f52e73accf33c385bcf51efc25b92156ba06
CrisTofani <cri.tofani@gmail.com>
2022-03-30 12:16:05 +0200

    reduces duplicated lines

--- src/components/Form/CreateActivationForm/CreateActivationForm.tsx
+++ src/components/Form/CreateActivationForm/CreateActivationForm.tsx
@@ -2,18 +2,12 @@ import { useHistory } from "react-router-dom";
 import React, { useState } from "react";
 import { tryCatch } from "fp-ts/lib/TaskEither";
 import { toError } from "fp-ts/lib/Either";
-import { Field, FieldArray, FieldArrayRenderProps, Form, Formik } from "formik";
-import { Button, Icon } from "design-react-kit";
 import { Severity, useTooltip } from "../../../context/tooltip";
 import Api from "../../../api/backoffice";
 import chainAxios from "../../../utils/chainAxios";
 import { ADMIN_PANEL_ACCESSI } from "../../../navigation/routes";
-import FormSection from "../FormSection";
 import { OrganizationWithReferents } from "../../../api/generated_backoffice";
-import InputField from "../FormField";
-import CustomErrorMessage from "../CustomErrorMessage";
-import PlusCircleIcon from "../../../assets/icons/plus-circle.svg";
-import CenteredLoading from "../../CenteredLoading";
+import ActivationForm from "../ActivationForm";
 
 const emptyInitialValues: OrganizationWithReferents = {
   keyOrganizationFiscalCode: "",
@@ -57,7 +51,7 @@ const CreateActivationForm = () => {
       .run();
 
   return (
-    <Formik
+    <ActivationForm
       initialValues={emptyInitialValues}
       onSubmit={values => {
         const newValues: OrganizationWithReferents = {
@@ -67,121 +61,8 @@ const CreateActivationForm = () => {
         setLoading(true);
         void createActivation(newValues);
       }}
-    >
-      {({ values }) => (
-        <Form autoComplete="off">
-          <FormSection
-            title={"Dati operatore"}
-            description={
-              "Tutti i campi sono obbligatori. Questi dati non sono visibili in app IO."
-            }
-            isVisible={false}
-          >
-            <InputField
-              htmlFor="organizationName"
-              title="Denominazione e ragione sociale Operatore"
-              description={"Inserire il nome completo dell'Operatore"}
-              required
-            >
-              <Field
-                id="organizationName"
-                name="organizationName"
-                type="text"
-              />
-              <CustomErrorMessage name="organizationName" />
-            </InputField>
-            <InputField
-              htmlFor="organizationFiscalCode"
-              title="Codice fiscale / Partita IVA"
-              required
-            >
-              <Field
-                id="organizationFiscalCode"
-                name="organizationFiscalCode"
-                type="text"
-              />
-              <CustomErrorMessage name="organizationFiscalCode" />
-            </InputField>
-            <InputField htmlFor="pec" title="Indirizzo PEC" required>
-              <Field id="pec" name="pec" type="text" />
-              <CustomErrorMessage name="pec" />
-            </InputField>
-          </FormSection>
-          <FormSection
-            title={"Utenti abilitati"}
-            description={
-              "Indicare il codice fiscale della persona o delle persone che devono accedere al portale per conto dell’operatore"
-            }
-            isVisible={false}
-          >
-            <FieldArray
-              name="referents"
-              render={({ push, remove }: FieldArrayRenderProps) => (
-                <>
-                  {values.referents.map((_: string, i: number) => (
-                    <div key={i}>
-                      <InputField
-                        htmlFor={`referents[${i}]`}
-                        title={`Codice fiscale utente ${i + 1}`}
-                        required
-                      >
-                        <div className="d-flex flex-row">
-                          <div className="col-10">
-                            <Field
-                              id={`referents[${i}]`}
-                              name={`referents[${i}]`}
-                              type="text"
-                            />
-                          </div>
-                          {i !== 0 && (
-                            <Button
-                              className="mr-4"
-                              sm
-                              color="link"
-                              tag="button"
-                              onClick={() => remove(i)}
-                            >
-                              <Icon icon="it-delete" size="sm" color="danger" />
-                            </Button>
-                          )}
-                        </div>
-                        <CustomErrorMessage name={`referents[${i}]`} />
-                      </InputField>
-                    </div>
-                  ))}
-                  <div className="mt-8 cursor-pointer" onClick={() => push("")}>
-                    <PlusCircleIcon className="mr-2" />
-                    <span className="text-base font-weight-semibold text-blue">
-                      Aggiungi
-                    </span>
-                  </div>
-                </>
-              )}
-            />
-            <div className="mt-10">
-              <Button
-                className="px-14 mr-4"
-                outline
-                color="primary"
-                tag="button"
-                onClick={() => history.push(ADMIN_PANEL_ACCESSI)}
-              >
-                Indietro
-              </Button>
-              <Button
-                type="submit"
-                className="px-14 mr-4"
-                color="primary"
-                disabled={loading}
-                tag="button"
-              >
-                {loading ? <CenteredLoading /> : "Salva"}
-              </Button>
-            </div>
-          </FormSection>
-        </Form>
-      )}
-    </Formik>
+      isSubmitting={loading}
+    />
   );
 };
 
--- src/components/Form/EditActivationForm/EditActivationForm.tsx
+++ src/components/Form/EditActivationForm/EditActivationForm.tsx
@@ -2,18 +2,13 @@ import { useHistory, useParams } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import { tryCatch } from "fp-ts/lib/TaskEither";
 import { toError } from "fp-ts/lib/Either";
-import { Field, FieldArray, FieldArrayRenderProps, Form, Formik } from "formik";
-import { Button, Icon } from "design-react-kit";
 import { Severity, useTooltip } from "../../../context/tooltip";
 import Api from "../../../api/backoffice";
 import chainAxios from "../../../utils/chainAxios";
 import { ADMIN_PANEL_ACCESSI } from "../../../navigation/routes";
-import FormSection from "../FormSection";
 import { OrganizationWithReferents } from "../../../api/generated_backoffice";
-import InputField from "../FormField";
-import CustomErrorMessage from "../CustomErrorMessage";
-import PlusCircleIcon from "../../../assets/icons/plus-circle.svg";
 import CenteredLoading from "../../CenteredLoading";
+import ActivationForm from "../ActivationForm";
 
 const emptyInitialValues: OrganizationWithReferents = {
   keyOrganizationFiscalCode: "",
@@ -28,16 +23,16 @@ const CreateActivationForm = () => {
   const { operatorFiscalCode } = useParams<{ operatorFiscalCode: string }>();
   const history = useHistory();
   const [loading, setLoading] = useState(false);
+  const [submitting, setSubmitting] = useState(false);
   const [initialValues, setInitialValues] = useState<OrganizationWithReferents>(
     emptyInitialValues
   );
   const { triggerTooltip } = useTooltip();
 
-  const throwErrorTooltip = () => {
+  const throwErrorTooltip = (message: string) => {
     triggerTooltip({
       severity: Severity.DANGER,
-      text:
-        "Errore durante la creazione dell'operatore, controllare i dati e riprovare"
+      text: message
     });
   };
 
@@ -50,11 +45,13 @@ const CreateActivationForm = () => {
       .map(response => response.data)
       .fold(
         () => {
-          setLoading(false);
-          throwErrorTooltip();
+          setSubmitting(false);
+          throwErrorTooltip(
+            "Errore durante la creazione dell'operatore, controllare i dati e riprovare"
+          );
         },
         () => {
-          setLoading(false);
+          setSubmitting(false);
           history.push(ADMIN_PANEL_ACCESSI);
         }
       )
@@ -70,7 +67,9 @@ const CreateActivationForm = () => {
       .fold(
         () => {
           setLoading(false);
-          throwErrorTooltip();
+          throwErrorTooltip(
+            "Errore durante la richiesta di dettaglio dell'operatore, riprovare"
+          );
         },
         (data: OrganizationWithReferents) => {
           setLoading(false);
@@ -89,131 +88,17 @@ const CreateActivationForm = () => {
   }
 
   return (
-    <Formik
+    <ActivationForm
       initialValues={initialValues}
       onSubmit={values => {
         const newValues: OrganizationWithReferents = {
-          ...values,
-          keyOrganizationFiscalCode: values.organizationFiscalCode
+          ...values
         };
-        setLoading(true);
+        setSubmitting(true);
         void createActivation(newValues);
       }}
-    >
-      {({ values }) => (
-        <Form autoComplete="off">
-          <FormSection
-            title={"Dati operatore"}
-            description={
-              "Tutti i campi sono obbligatori. Questi dati non sono visibili in app IO."
-            }
-            isVisible={false}
-          >
-            <InputField
-              htmlFor="organizationName"
-              title="Denominazione e ragione sociale Operatore"
-              description={"Inserire il nome completo dell'Operatore"}
-              required
-            >
-              <Field
-                id="organizationName"
-                name="organizationName"
-                type="text"
-              />
-              <CustomErrorMessage name="organizationName" />
-            </InputField>
-            <InputField
-              htmlFor="organizationFiscalCode"
-              title="Codice fiscale / Partita IVA"
-              required
-            >
-              <Field
-                id="organizationFiscalCode"
-                name="organizationFiscalCode"
-                type="text"
-              />
-              <CustomErrorMessage name="organizationFiscalCode" />
-            </InputField>
-            <InputField htmlFor="pec" title="Indirizzo PEC" required>
-              <Field id="pec" name="pec" type="text" />
-              <CustomErrorMessage name="pec" />
-            </InputField>
-          </FormSection>
-          <FormSection
-            title={"Utenti abilitati"}
-            description={
-              "Indicare il codice fiscale della persona o delle persone che devono accedere al portale per conto dell’operatore"
-            }
-            isVisible={false}
-          >
-            <FieldArray
-              name="referents"
-              render={({ push, remove }: FieldArrayRenderProps) => (
-                <>
-                  {values.referents.map((_: string, i: number) => (
-                    <div key={i}>
-                      <InputField
-                        htmlFor={`referents[${i}]`}
-                        title={`Codice fiscale utente ${i + 1}`}
-                        required
-                      >
-                        <div className="d-flex flex-row">
-                          <div className="col-10">
-                            <Field
-                              id={`referents[${i}]`}
-                              name={`referents[${i}]`}
-                              type="text"
-                            />
-                          </div>
-                          {i !== 0 && (
-                            <Button
-                              className="mr-4"
-                              sm
-                              color="link"
-                              tag="button"
-                              onClick={() => remove(i)}
-                            >
-                              <Icon icon="it-delete" size="sm" color="danger" />
-                            </Button>
-                          )}
-                        </div>
-                        <CustomErrorMessage name={`referents[${i}]`} />
-                      </InputField>
-                    </div>
-                  ))}
-                  <div className="mt-8 cursor-pointer" onClick={() => push("")}>
-                    <PlusCircleIcon className="mr-2" />
-                    <span className="text-base font-weight-semibold text-blue">
-                      Aggiungi
-                    </span>
-                  </div>
-                </>
-              )}
-            />
-            <div className="mt-10">
-              <Button
-                className="px-14 mr-4"
-                outline
-                color="primary"
-                tag="button"
-                onClick={() => history.push(ADMIN_PANEL_ACCESSI)}
-              >
-                Indietro
-              </Button>
-              <Button
-                type="submit"
-                className="px-14 mr-4"
-                color="primary"
-                disabled={loading}
-                tag="button"
-              >
-                {loading ? <CenteredLoading /> : "Salva"}
-              </Button>
-            </div>
-          </FormSection>
-        </Form>
-      )}
-    </Formik>
+      isSubmitting={submitting}
+    />
   );
 };
 
