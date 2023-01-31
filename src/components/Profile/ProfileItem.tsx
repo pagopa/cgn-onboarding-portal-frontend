e11fd7cb220c865f5b4fdfbfad80668899b1eefb
Cristiano <cri.tofani@gmail.com>
2023-01-31 16:29:11 +0100

    Fixes wrap on link if it overflows

--- src/components/Profile/ProfileItem.tsx
+++ src/components/Profile/ProfileItem.tsx
@@ -9,7 +9,17 @@ type Props = {
 const ProfileItem = ({ className = "", label, value }: Props) => (
   <tr>
     <td className={`${className} px-0 text-gray border-bottom-0`}>{label}</td>
-    <td className={`${className} border-bottom-0`}>{value}</td>
+    <td
+      className={`${className} border-bottom-0`}
+      style={{
+        maxWidth: "250px",
+        whiteSpace: "nowrap",
+        overflow: "hidden",
+        textOverflow: "ellipsis"
+      }}
+    >
+      {value}
+    </td>
   </tr>
 );
 
