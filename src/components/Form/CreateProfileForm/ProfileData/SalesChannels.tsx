ebcf02bf6d38f7020f7c49762275855318f90d6a
Frederik Batuna <gobi301@gmail.com>
2024-09-23 11:29:10 +0200

    fix: missing continue button for phisical venue

--- src/components/Form/CreateProfileForm/ProfileData/SalesChannels.tsx
+++ src/components/Form/CreateProfileForm/ProfileData/SalesChannels.tsx
@@ -30,6 +30,8 @@ const SalesChannels = ({ entityType, children }: Props) => {
   const hasOnlineOrBothChannels =
     formValues.salesChannel?.channelType === "OnlineChannel" ||
     formValues.salesChannel?.channelType === "BothChannels";
+  const hasWebsite =
+    hasOnlineOrBothChannels || entityType === EntityType.PublicAdministration;
   return (
     // const autocomplete = async (q: any) =>
     //   await tryCatch(
@@ -67,210 +69,201 @@ const SalesChannels = ({ entityType, children }: Props) => {
         name="salesChannel.addresses"
         render={arrayHelpers => (
           <>
-            {formValues.salesChannel?.addresses?.map(
-              (_: any, index: number) => (
-                <FormSection
-                  key={index}
-                  title={
-                    index + 1 >= 2 ? `Indirizzo ${index + 1}` : `Indirizzo`
+            {formValues.salesChannel?.addresses?.map((_: any, index, array) => (
+              <FormSection
+                key={index}
+                title={index + 1 >= 2 ? `Indirizzo ${index + 1}` : `Indirizzo`}
+                description={(() => {
+                  switch (entityType) {
+                    case EntityType.Private:
+                      return "Inserisci l'indirizzo completo della sede. Servirà agli utenti per usufruire delle agevolazioni del tuo operatore, se richiedono una sede fisica.";
+                    case EntityType.PublicAdministration:
+                      return "Inserisci l'indirizzo completo della sede. Servirà agli utenti per usufruire delle opportunità del tuo operatore, se richiedono una sede fisica.";
                   }
-                  description={(() => {
-                    switch (entityType) {
-                      case EntityType.Private:
-                        return "Inserisci l'indirizzo completo della sede. Servirà agli utenti per usufruire delle agevolazioni del tuo operatore, se richiedono una sede fisica.";
-                      case EntityType.PublicAdministration:
-                        return "Inserisci l'indirizzo completo della sede. Servirà agli utenti per usufruire delle opportunità del tuo operatore, se richiedono una sede fisica.";
-                    }
-                  })()}
-                  required={(() => {
-                    switch (entityType) {
-                      case EntityType.Private: {
-                        if (formValues.salesChannel?.allNationalAddresses) {
-                          return false;
-                        }
-                        return (
-                          formValues.salesChannel.channelType ===
-                            SalesChannelType.OfflineChannel && index + 1 === 1
-                        );
+                })()}
+                required={(() => {
+                  switch (entityType) {
+                    case EntityType.Private: {
+                      if (formValues.salesChannel?.allNationalAddresses) {
+                        return false;
                       }
-                      default:
-                      case EntityType.PublicAdministration: {
-                        return (
-                          formValues.salesChannel.channelType ===
-                          "OfflineChannel"
-                        );
-                      }
+                      return (
+                        formValues.salesChannel.channelType ===
+                          SalesChannelType.OfflineChannel && index + 1 === 1
+                      );
                     }
-                  })()}
-                  isVisible
-                >
-                  {index === 0 && (
-                    <ToggleField
-                      small={false}
-                      htmlFor="allNationalAddresses"
-                      text={(() => {
-                        switch (entityType) {
-                          case EntityType.Private:
-                            return "Rappresenti un franchising e vuoi che le agevolazioni valgano in tutti i punti vendita presenti sul territorio nazionale?";
-                          default:
-                          case EntityType.PublicAdministration:
-                            return "Rappresenti un ente e vuoi che le opportunità valgano in tutti i punti vendita presenti sul territorio nazionale?";
-                        }
-                      })()}
-                    >
-                      <Field
-                        id="allNationalAddresses"
-                        name="salesChannel.allNationalAddresses"
-                        type="checkbox"
-                        onClick={() =>
-                          formikContext.setFieldValue(
-                            "salesChannel.addresses",
-                            [
-                              {
-                                street: "",
-                                zipCode: "",
-                                city: "",
-                                district: ""
-                              }
-                            ]
-                          )
-                        }
-                      />
-                    </ToggleField>
-                  )}
+                    default:
+                    case EntityType.PublicAdministration: {
+                      return (
+                        formValues.salesChannel.channelType === "OfflineChannel"
+                      );
+                    }
+                  }
+                })()}
+                isVisible
+              >
+                {index === 0 && (
+                  <ToggleField
+                    small={false}
+                    htmlFor="allNationalAddresses"
+                    text={(() => {
+                      switch (entityType) {
+                        case EntityType.Private:
+                          return "Rappresenti un franchising e vuoi che le agevolazioni valgano in tutti i punti vendita presenti sul territorio nazionale?";
+                        default:
+                        case EntityType.PublicAdministration:
+                          return "Rappresenti un ente e vuoi che le opportunità valgano in tutti i punti vendita presenti sul territorio nazionale?";
+                      }
+                    })()}
+                  >
+                    <Field
+                      id="allNationalAddresses"
+                      name="salesChannel.allNationalAddresses"
+                      type="checkbox"
+                      onClick={() =>
+                        formikContext.setFieldValue("salesChannel.addresses", [
+                          {
+                            street: "",
+                            zipCode: "",
+                            city: "",
+                            district: ""
+                          }
+                        ])
+                      }
+                    />
+                  </ToggleField>
+                )}
 
-                  <div key={index}>
-                    {!!index &&
-                      formValues.salesChannel?.allNationalAddresses ===
-                        false && (
-                        <Icon
-                          icon="it-close"
-                          style={{
-                            position: "absolute",
-                            right: "0",
-                            top: "40px",
-                            cursor: "pointer"
-                          }}
-                          onClick={() => arrayHelpers.remove(index)}
-                        />
-                      )}
-                    {formValues.salesChannel?.allNationalAddresses ===
-                      false && (
-                      // <div className="mt-10 row">
-                      //   <div className="col-7">
-                      //     <AsyncSelect
-                      //       placeholder="Inserisci indirizzo"
-                      //       cacheOptions
-                      //       loadOptions={autocomplete}
-                      //       noOptionsMessage={() => "Nessun risultato"}
-                      //       value={formValues.salesChannel.addresses[index]}
-                      //       onChange={(e: any) =>
-                      //         setFieldValue(
-                      //           `salesChannel.addresses[${index}]`,
-                      //           e
-                      //         )
-                      //       }
-                      //     />
-                      //   </div>
-                      // </div>
-                      <>
-                        <div className="mt-10 row">
-                          <div className="col-7">
-                            <InputFieldMultiple
-                              htmlFor="street"
-                              title="Indirizzo"
-                            >
-                              <Field
-                                id="street"
-                                name={`salesChannel.addresses[${index}].street`}
-                                type="text"
-                              />
-                              <CustomErrorMessage
-                                name={`salesChannel.addresses[${index}].street`}
-                              />
-                            </InputFieldMultiple>
-                          </div>
-                          <div className="col-2 offset-1">
-                            <InputFieldMultiple htmlFor="zipCode" title="CAP">
-                              <Field
-                                id="zipCode"
-                                name={`salesChannel.addresses[${index}].zipCode`}
-                                type="text"
-                                placeholder="Inserisci il CAP"
-                              />
-                              <CustomErrorMessage
-                                name={`salesChannel.addresses[${index}].zipCode`}
-                              />
-                            </InputFieldMultiple>
-                          </div>
-                        </div>
-                        <div className="mt-10 row">
-                          <div className="col-6">
-                            <InputFieldMultiple htmlFor="city" title="Città">
-                              <Field
-                                id="city"
-                                name={`salesChannel.addresses[${index}].city`}
-                                type="text"
-                                placeholder="Inserisci la città"
-                              />
-                              <CustomErrorMessage
-                                name={`salesChannel.addresses[${index}].city`}
-                              />
-                            </InputFieldMultiple>
-                          </div>
-                          <div className="col-3 offset-1">
-                            <InputFieldMultiple
-                              htmlFor="district"
-                              title="Provincia"
-                            >
-                              <Field
-                                maxLength={2}
-                                id="district"
-                                name={`salesChannel.addresses[${index}].district`}
-                                type="text"
-                                placeholder="Inserisci la provincia"
-                              />
-                              <CustomErrorMessage
-                                name={`salesChannel.addresses[${index}].district`}
-                              />
-                            </InputFieldMultiple>
-                          </div>
-                        </div>
-                      </>
+                <div key={index}>
+                  {!!index &&
+                    formValues.salesChannel?.allNationalAddresses === false && (
+                      <Icon
+                        icon="it-close"
+                        style={{
+                          position: "absolute",
+                          right: "0",
+                          top: "40px",
+                          cursor: "pointer"
+                        }}
+                        onClick={() => arrayHelpers.remove(index)}
+                      />
                     )}
-                    {formValues.salesChannel?.addresses?.length ===
-                      index + 1 && (
-                      <>
-                        {formValues.salesChannel?.allNationalAddresses ===
-                          false && (
-                          <div
-                            className="mt-8 cursor-pointer"
-                            onClick={() =>
-                              arrayHelpers.push({
-                                street: "",
-                                zipCode: "",
-                                city: "",
-                                district: ""
-                              })
-                            }
+                  {formValues.salesChannel?.allNationalAddresses === false && (
+                    // <div className="mt-10 row">
+                    //   <div className="col-7">
+                    //     <AsyncSelect
+                    //       placeholder="Inserisci indirizzo"
+                    //       cacheOptions
+                    //       loadOptions={autocomplete}
+                    //       noOptionsMessage={() => "Nessun risultato"}
+                    //       value={formValues.salesChannel.addresses[index]}
+                    //       onChange={(e: any) =>
+                    //         setFieldValue(
+                    //           `salesChannel.addresses[${index}]`,
+                    //           e
+                    //         )
+                    //       }
+                    //     />
+                    //   </div>
+                    // </div>
+                    <>
+                      <div className="mt-10 row">
+                        <div className="col-7">
+                          <InputFieldMultiple
+                            htmlFor="street"
+                            title="Indirizzo"
                           >
-                            <PlusCircleIcon className="mr-2" />
-                            <span className="text-base font-weight-semibold text-blue">
-                              Aggiungi un indirizzo
-                            </span>
-                          </div>
-                        )}
-                      </>
-                    )}
-                  </div>
-                </FormSection>
-              )
-            )}
+                            <Field
+                              id="street"
+                              name={`salesChannel.addresses[${index}].street`}
+                              type="text"
+                            />
+                            <CustomErrorMessage
+                              name={`salesChannel.addresses[${index}].street`}
+                            />
+                          </InputFieldMultiple>
+                        </div>
+                        <div className="col-2 offset-1">
+                          <InputFieldMultiple htmlFor="zipCode" title="CAP">
+                            <Field
+                              id="zipCode"
+                              name={`salesChannel.addresses[${index}].zipCode`}
+                              type="text"
+                              placeholder="Inserisci il CAP"
+                            />
+                            <CustomErrorMessage
+                              name={`salesChannel.addresses[${index}].zipCode`}
+                            />
+                          </InputFieldMultiple>
+                        </div>
+                      </div>
+                      <div className="mt-10 row">
+                        <div className="col-6">
+                          <InputFieldMultiple htmlFor="city" title="Città">
+                            <Field
+                              id="city"
+                              name={`salesChannel.addresses[${index}].city`}
+                              type="text"
+                              placeholder="Inserisci la città"
+                            />
+                            <CustomErrorMessage
+                              name={`salesChannel.addresses[${index}].city`}
+                            />
+                          </InputFieldMultiple>
+                        </div>
+                        <div className="col-3 offset-1">
+                          <InputFieldMultiple
+                            htmlFor="district"
+                            title="Provincia"
+                          >
+                            <Field
+                              maxLength={2}
+                              id="district"
+                              name={`salesChannel.addresses[${index}].district`}
+                              type="text"
+                              placeholder="Inserisci la provincia"
+                            />
+                            <CustomErrorMessage
+                              name={`salesChannel.addresses[${index}].district`}
+                            />
+                          </InputFieldMultiple>
+                        </div>
+                      </div>
+                    </>
+                  )}
+                  {formValues.salesChannel?.addresses?.length === index + 1 && (
+                    <>
+                      {formValues.salesChannel?.allNationalAddresses ===
+                        false && (
+                        <div
+                          className="mt-8 cursor-pointer"
+                          onClick={() =>
+                            arrayHelpers.push({
+                              street: "",
+                              zipCode: "",
+                              city: "",
+                              district: ""
+                            })
+                          }
+                        >
+                          <PlusCircleIcon className="mr-2" />
+                          <span className="text-base font-weight-semibold text-blue">
+                            Aggiungi un indirizzo
+                          </span>
+                        </div>
+                      )}
+                      {!hasWebsite && index === array.length - 1
+                        ? children
+                        : null}
+                    </>
+                  )}
+                </div>
+              </FormSection>
+            ))}
           </>
         )}
       />
-      {(hasOnlineOrBothChannels ||
-        entityType === EntityType.PublicAdministration) && (
+      {hasWebsite && (
         <FormSection
           title="Sito web"
           description={(() => {
