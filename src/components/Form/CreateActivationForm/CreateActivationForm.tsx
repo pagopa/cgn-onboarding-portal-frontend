ec38385d57ba17623e8d13cfbcaebd2439705109
Frederik Batuna <gobi301@gmail.com>
2024-11-22 12:28:09 +0100

    add: ajax call adapter proposal

--- src/components/Form/CreateActivationForm/CreateActivationForm.tsx
+++ src/components/Form/CreateActivationForm/CreateActivationForm.tsx
@@ -1,6 +1,6 @@
 import { useHistory } from "react-router-dom";
 import React, { useState } from "react";
-import { AxiosError } from "axios";
+import { AxiosError, AxiosResponse } from "axios";
 import { Severity, useTooltip } from "../../../context/tooltip";
 import Api from "../../../api/backoffice";
 import { ADMIN_PANEL_ACCESSI } from "../../../navigation/routes";
@@ -23,6 +23,116 @@ const CreateActivationForm = () => {
   const history = useHistory();
   const [loading, setLoading] = useState(false);
   const { triggerTooltip } = useTooltip();
+
+  /**
+   * given a result of an ajax call, returns a promise that always resolves, see return type
+   * this mimics Future<Either<Err, Res>> composition (like in functional languages) that is more practical and conserves error type
+   */
+  type StatusCodeOk = 200 | 201 | 204;
+  type StatusCodeKo = 400 | 403 | 404 | 409 | 404 | 500;
+  async function decodeAxiosA<Data>(
+    axiosOutcome: Promise<AxiosResponse<Data, unknown>>
+  ): Promise<
+    | { status: StatusCodeOk; data: Data }
+    | { status: StatusCodeKo; data: unknown }
+    | unknown // stands for errors that has not a http response or other erros that might be returned by axios
+  > {
+    try {
+      const outcome = await axiosOutcome;
+      if (outcome instanceof AxiosError) {
+        if (outcome.response) {
+          return {
+            status: outcome.response.status as StatusCodeKo,
+            data: outcome.response.data
+          };
+        } else {
+          return outcome;
+        }
+      } else {
+        return {
+          status: outcome.status as StatusCodeOk,
+          data: outcome.data
+        };
+      }
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async function exampleUsageDecodeAxiosA(
+    organization: OrganizationWithReferents
+  ) {
+    const response = await Api.AttributeAuthority.upsertOrganization(
+      organization
+    );
+    if (response.status === 200) {
+      // everything all right
+    } else if (response.status === 201) {
+      // everything all right, but show a different visual feedback
+    } else if (
+      response.status === 400 ||
+      response.data === "BUSINESS_LOGIC_ERROR_CODE"
+    ) {
+      // show a tooltip
+    } else {
+      // show a generic error message
+    }
+  }
+
+  /**
+   * given a result of an ajax call, returns a promise that resolves if status code 2xx,
+   * otherwise rejects with AxiosError that might contain a response with a status code.
+   * this aligns more with javascript stile promises, we loses error type, must be enclosed with try/catch statement with rethrow
+   * and the programmer must remember to check for http error codes in the AxiosError
+   */
+  async function decodeAxiosB<Data>(
+    axiosOutcome: Promise<AxiosResponse<Data, unknown>>
+  ): Promise<{ status: StatusCodeOk; data: Data }> {
+    const outcome = await axiosOutcome;
+    if (outcome instanceof AxiosError) {
+      throw outcome;
+    } else {
+      return {
+        status: outcome.status as StatusCodeOk,
+        data: outcome.data
+      };
+    }
+  }
+
+  type AxiosResponseLike = {
+    status: number;
+    data: unknown;
+  };
+  class AxiosErrorLike extends Error {
+    response?: AxiosResponseLike;
+    constructor(response?: AxiosResponseLike) {
+      super();
+      this.response = response;
+    }
+  }
+
+  async function exampleUsageDecodeAxiosB(
+    organization: OrganizationWithReferents
+  ) {
+    try {
+      const response = await Api.AttributeAuthority.upsertOrganization(
+        organization
+      );
+      if (response.status === 200) {
+        // everything all right
+      } else if (response.status === 201) {
+        // everything all right, but show a different visual feedback
+      } else {
+        throw new AxiosErrorLike(response);
+      }
+    } catch (error) {
+      if (error.response?.status === 400) {
+        // show a tooltip
+      } else {
+        // show a generic error message
+      }
+    }
+  }
 
   const createActivation = async (organization: OrganizationWithReferents) => {
     try {
