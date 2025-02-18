373ab07e393a79ec7a957003a92d6afd07a7b55e
Frederik Batuna <gobi301@gmail.com>
2025-02-18 12:29:46 +0100

    review

--- src/components/Form/CreateActivationForm/CreateActivationForm.tsx
+++ src/components/Form/CreateActivationForm/CreateActivationForm.tsx
@@ -22,45 +22,46 @@ const CreateActivationForm = () => {
   const history = useHistory();
   const { triggerTooltip } = useTooltip();
 
-  const upsertOrganizationMutation = remoteData.Backoffice.AttributeAuthority.upsertOrganization.useMutation(
-    {
-      onSuccess() {
-        history.push(ADMIN_PANEL_ACCESSI);
-      },
-      async onError(error) {
-        if (
-          error.status === 400 &&
-          error.response?.data === "CANNOT_BIND_MORE_THAN_TEN_ORGANIZATIONS"
-        ) {
-          triggerTooltip({
-            severity: Severity.DANGER,
-            text:
-              "Uno o più utenti abilitati gestiscono già 10 operatori e non possono gestirne altri. Controlla e riprova."
-          });
-        } else {
-          triggerTooltip({
-            severity: Severity.DANGER,
-            text:
-              "Errore durante la creazione dell'operatore, controllare i dati e riprovare"
-          });
-        }
+  const {
+    mutate,
+    isLoading
+  } = remoteData.Backoffice.AttributeAuthority.upsertOrganization.useMutation({
+    onSuccess() {
+      history.push(ADMIN_PANEL_ACCESSI);
+    },
+    async onError(error) {
+      if (
+        error.status === 400 &&
+        error.response?.data === "CANNOT_BIND_MORE_THAN_TEN_ORGANIZATIONS"
+      ) {
+        triggerTooltip({
+          severity: Severity.DANGER,
+          text:
+            "Uno o più utenti abilitati gestiscono già 10 operatori e non possono gestirne altri. Controlla e riprova."
+        });
+      } else {
+        triggerTooltip({
+          severity: Severity.DANGER,
+          text:
+            "Errore durante la creazione dell'operatore, controllare i dati e riprovare"
+        });
       }
     }
-  );
+  });
 
   return (
     <ActivationForm
       enableReinitialize={false}
       initialValues={emptyInitialValues}
       onSubmit={values => {
-        upsertOrganizationMutation.mutate({
+        mutate({
           body: {
             ...values,
             keyOrganizationFiscalCode: values.organizationFiscalCode
           }
         });
       }}
-      isSubmitting={upsertOrganizationMutation.isLoading}
+      isSubmitting={isLoading}
       canChangeEntityType={true}
     />
   );
