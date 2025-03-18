251f90c202db07f6419cb07b418c2808a0e2f52c
Alessandro <alessandro.izzo@pagopa.it>
2025-03-18 12:34:07 +0100

    chore: Disabled maintenance banner

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -124,8 +124,7 @@ const Login = () => {
 
 export default Login;
 
-const MAINTENANCE_BANNER: undefined | "short-downtime" | "long-downtime" =
-  "short-downtime";
+const MAINTENANCE_BANNER: undefined | "short-downtime" | "long-downtime" = undefined;
 
 function Alert({ title }: { title: string }) {
   return (
