0568c80fee76f689f219484e651ee1af2d792f0f
Frederik Batuna <gobi301@gmail.com>
2025-05-20 16:13:43 +0200

    chore: [IOBP-1635] admin portal add error message for empty bucket list (#305)
    * chore: add specific errore message
    
    * review fix
    
    * refactor

--- src/components/OperatorConvention/BucketCodeModal.tsx
+++ src/components/OperatorConvention/BucketCodeModal.tsx
@@ -17,27 +17,35 @@ const BucketCodeModal = ({
   agreementId,
   discountId
 }: Props) => {
-  const { data, isLoading, isError } =
+  const { data, isLoading, isError, error } =
     remoteData.Backoffice.Discount.getDiscountBucketCode.useQuery(
       {
         agreementId,
         discountId
       },
-      { enabled: isOpen }
+      { enabled: isOpen, retry: false }
     );
 
+  const renderContent = () => {
+    if (isLoading) {
+      return <CenteredLoading />;
+    }
+    if (data?.code && !isError) {
+      return data.code;
+    }
+    if (
+      error?.status === 400 &&
+      error.response?.data ===
+        "CANNOT_RETRIEVE_BUCKET_CODE_FROM_DISCOUNT_WITH_EMPTY_BUCKET"
+    ) {
+      return "I codici sconto disponibili sono terminati. L’operatore deve caricare una nuova lista di codici per poter procedere.";
+    }
+    return "Non è stato possibile caricare il codice";
+  };
   return (
     <Modal isOpen={isOpen} toggle={toggle} size="md">
       <ModalHeader toggle={toggle}>Codice Sconto dalla lista</ModalHeader>
-      <ModalBody>
-        {isLoading ? (
-          <CenteredLoading />
-        ) : data?.code && !isError ? (
-          data.code
-        ) : (
-          "Non è stato possibile caricare il codice"
-        )}
-      </ModalBody>
+      <ModalBody>{renderContent()}</ModalBody>
       <ModalFooter className="d-flex flex-column">
         <Button
           color="primary"
