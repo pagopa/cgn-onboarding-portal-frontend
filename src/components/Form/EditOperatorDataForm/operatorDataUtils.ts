135968e597dd48331fb7d4fa7efba8edd9467f73
Frederik Batuna <gobi301@gmail.com>
2025-05-22 10:15:09 +0200

    post merge fix

--- src/components/Form/EditOperatorDataForm/operatorDataUtils.ts
+++ src/components/Form/EditOperatorDataForm/operatorDataUtils.ts
@@ -59,7 +59,7 @@ function getSalesChannel(
       return {
         ...OfflineChannel,
         addresses:
-          EmptyAddresses.is(OfflineChannel.addresses) ||
+          EmptyAddresses.isValidSync(OfflineChannel.addresses) ||
           OfflineChannel.allNationalAddresses
             ? []
             : OfflineChannel.addresses.map((add: any) => ({
@@ -72,7 +72,7 @@ function getSalesChannel(
       return {
         ...salesChannel,
         addresses:
-          EmptyAddresses.is(salesChannel.addresses) ||
+          EmptyAddresses.isValidSync(salesChannel.addresses) ||
           salesChannel.allNationalAddresses
             ? []
             : salesChannel.addresses.map((add: any) => ({
