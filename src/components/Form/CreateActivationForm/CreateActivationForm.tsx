e075d8d3b96c29597d9fdc6a9fc6d24a24d44c1d
Frederik Batuna <gobi301@gmail.com>
2025-07-22 17:01:53 +0200

    deduplicate activation form

--- src/components/Form/CreateActivationForm/CreateActivationForm.tsx
+++ src/components/Form/CreateActivationForm/CreateActivationForm.tsx
@@ -1,27 +1,27 @@
-import { useHistory } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import { Severity, useTooltip } from "../../../context/tooltip";
 import { remoteData } from "../../../api/common";
 import { ADMIN_PANEL_ACCESSI } from "../../../navigation/routes";
-import {
-  EntityType,
-  OrganizationWithReferents
-} from "../../../api/generated_backoffice";
+import { OrganizationWithReferents } from "../../../api/generated_backoffice";
 import ActivationForm from "../ActivationForm";
+import CenteredLoading from "../../CenteredLoading/CenteredLoading";
 
 const emptyInitialValues: OrganizationWithReferents = {
   keyOrganizationFiscalCode: "",
   organizationFiscalCode: "",
   organizationName: "",
-  entityType: EntityType.Private,
+  insertedAt: "",
   pec: "",
-  referents: [""]
+  referents: [""],
+  entityType: undefined
 };
 
-const CreateActivationForm = () => {
+const CreateEditActivationForm = () => {
+  const { operatorFiscalCode } = useParams<{ operatorFiscalCode: string }>();
   const history = useHistory();
   const { triggerTooltip } = useTooltip();
 
-  const { mutate, isPending } =
+  const upsertActivationMutation =
     remoteData.Backoffice.AttributeAuthority.upsertOrganization.useMutation({
       onSuccess() {
         history.push(ADMIN_PANEL_ACCESSI);
@@ -38,28 +38,45 @@ const CreateActivationForm = () => {
         } else {
           triggerTooltip({
             severity: Severity.DANGER,
-            text: "Errore durante la creazione dell'operatore, controllare i dati e riprovare"
+            text: operatorFiscalCode
+              ? "Errore durante la modifica dell'operatore, controllare i dati e riprovare"
+              : "Errore durante la creazione dell'operatore, controllare i dati e riprovare"
           });
         }
       }
     });
 
+  const organizationQuery =
+    remoteData.Backoffice.AttributeAuthority.getOrganization.useQuery({
+      keyOrganizationFiscalCode: operatorFiscalCode
+    });
+
+  const initialValues = organizationQuery.data ?? emptyInitialValues;
+
+  if (organizationQuery.isPending) {
+    return <CenteredLoading />;
+  }
+
   return (
     <ActivationForm
-      enableReinitialize={false}
-      initialValues={emptyInitialValues}
+      enableReinitialize={true}
+      initialValues={initialValues}
       onSubmit={values => {
-        mutate({
-          body: {
-            ...values,
-            keyOrganizationFiscalCode: values.organizationFiscalCode
-          }
-        });
+        if (operatorFiscalCode) {
+          upsertActivationMutation.mutate({ body: values });
+        } else {
+          upsertActivationMutation.mutate({
+            body: {
+              ...values,
+              keyOrganizationFiscalCode: values.organizationFiscalCode
+            }
+          });
+        }
       }}
-      isSubmitting={isPending}
+      isSubmitting={upsertActivationMutation.isPending}
       canChangeEntityType={true}
     />
   );
 };
 
-export default CreateActivationForm;
+export default CreateEditActivationForm;
--- src/components/Form/EditActivationForm/EditActivationForm.tsx
+++ src/components/Form/EditActivationForm/EditActivationForm.tsx
@@ -1,11 +1,10 @@
 import { useHistory, useParams } from "react-router-dom";
-import { useCallback, useEffect } from "react";
 import { Severity, useTooltip } from "../../../context/tooltip";
 import { remoteData } from "../../../api/common";
 import { ADMIN_PANEL_ACCESSI } from "../../../navigation/routes";
 import { OrganizationWithReferents } from "../../../api/generated_backoffice";
-import CenteredLoading from "../../CenteredLoading/CenteredLoading";
 import ActivationForm from "../ActivationForm";
+import CenteredLoading from "../../CenteredLoading/CenteredLoading";
 
 const emptyInitialValues: OrganizationWithReferents = {
   keyOrganizationFiscalCode: "",
@@ -17,22 +16,12 @@ const emptyInitialValues: OrganizationWithReferents = {
   entityType: undefined
 };
 
-const CreateActivationForm = () => {
+const CreateEditActivationForm = () => {
   const { operatorFiscalCode } = useParams<{ operatorFiscalCode: string }>();
   const history = useHistory();
   const { triggerTooltip } = useTooltip();
 
-  const throwErrorTooltip = useCallback(
-    (message: string) => {
-      triggerTooltip({
-        severity: Severity.DANGER,
-        text: message
-      });
-    },
-    [triggerTooltip]
-  );
-
-  const createActivationMutation =
+  const upsertActivationMutation =
     remoteData.Backoffice.AttributeAuthority.upsertOrganization.useMutation({
       onSuccess() {
         history.push(ADMIN_PANEL_ACCESSI);
@@ -42,35 +31,29 @@ const CreateActivationForm = () => {
           error.status === 400 &&
           error.response?.data === "CANNOT_BIND_MORE_THAN_TEN_ORGANIZATIONS"
         ) {
-          throwErrorTooltip(
-            "Uno o più utenti abilitati gestiscono già 10 operatori e non possono gestirne altri. Controlla e riprova."
-          );
+          triggerTooltip({
+            severity: Severity.DANGER,
+            text: "Uno o più utenti abilitati gestiscono già 10 operatori e non possono gestirne altri. Controlla e riprova."
+          });
         } else {
-          throwErrorTooltip(
-            "Errore durante la modifica dell'operatore, controllare i dati e riprovare"
-          );
+          triggerTooltip({
+            severity: Severity.DANGER,
+            text: operatorFiscalCode
+              ? "Errore durante la modifica dell'operatore, controllare i dati e riprovare"
+              : "Errore durante la creazione dell'operatore, controllare i dati e riprovare"
+          });
         }
       }
     });
-  const createActivation = (organization: OrganizationWithReferents) => {
-    createActivationMutation.mutate({ body: organization });
-  };
 
-  const { data, error, isPending } =
+  const organizationQuery =
     remoteData.Backoffice.AttributeAuthority.getOrganization.useQuery({
       keyOrganizationFiscalCode: operatorFiscalCode
     });
-  useEffect(() => {
-    if (error) {
-      throwErrorTooltip(
-        "Errore durante la richiesta di dettaglio dell'operatore, riprovare"
-      );
-    }
-  }, [data, error, throwErrorTooltip]);
 
-  const initialValues = data ?? emptyInitialValues;
+  const initialValues = organizationQuery.data ?? emptyInitialValues;
 
-  if (isPending) {
+  if (organizationQuery.isPending) {
     return <CenteredLoading />;
   }
 
@@ -78,11 +61,22 @@ const CreateActivationForm = () => {
     <ActivationForm
       enableReinitialize={true}
       initialValues={initialValues}
-      onSubmit={createActivation}
-      isSubmitting={createActivationMutation.isPending}
-      canChangeEntityType={false}
+      onSubmit={values => {
+        if (operatorFiscalCode) {
+          upsertActivationMutation.mutate({ body: values });
+        } else {
+          upsertActivationMutation.mutate({
+            body: {
+              ...values,
+              keyOrganizationFiscalCode: values.organizationFiscalCode
+            }
+          });
+        }
+      }}
+      isSubmitting={upsertActivationMutation.isPending}
+      canChangeEntityType={!operatorFiscalCode}
     />
   );
 };
 
-export default CreateActivationForm;
+export default CreateEditActivationForm;
