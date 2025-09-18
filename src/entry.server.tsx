eb91b51664e4157899c7435c915933e022482368
Frederik Batuna <gobi301@gmail.com>
2025-09-18 15:08:19 +0200

    separate agoin to devDependencies and dependencies

--- src/entry.server.tsx
+++ src/entry.server.tsx
@@ -26,21 +26,18 @@ export default function handleRequest(
   return new Promise((resolve, reject) => {
     let shellRendered = false;
     let userAgent = request.headers.get("user-agent");
-
     // Ensure requests from bots and SPA Mode renders wait for all content to load before responding
     // https://react.dev/reference/react-dom/server/renderToPipeableStream#waiting-for-all-content-to-load-for-crawlers-and-static-generation
     let readyOption: keyof RenderToPipeableStreamOptions =
       (userAgent && isbot(userAgent)) || routerContext.isSpaMode
         ? "onAllReady"
         : "onShellReady";
-
     // Abort the rendering stream after the `streamTimeout` so it has time to
     // flush down the rejected boundaries
     let timeoutId: ReturnType<typeof setTimeout> | undefined = setTimeout(
       () => abort(),
       streamTimeout + 1000
     );
-
     const { pipe, abort } = renderToPipeableStream(
       <ServerRouter context={routerContext} url={request.url} />,
       {
@@ -55,11 +52,8 @@ export default function handleRequest(
             }
           });
           const stream = createReadableStreamFromReadable(body);
-
           responseHeaders.set("Content-Type", "text/html");
-
           pipe(body);
-
           resolve(
             new Response(stream, {
               headers: responseHeaders,
