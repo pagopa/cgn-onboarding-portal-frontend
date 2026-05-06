6b409765ca2a49f00dc95e215475a59d5ad7b827
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2026-05-06 13:07:56 +0200

    chore: removed unused api key

--- src/utils/env-variables.ts
+++ src/utils/env-variables.ts
@@ -9,7 +9,6 @@ export const envVariablesSchema = z.object({
   CGN_MSAL_AUTHORITY: z.string().nonempty(),
   CGN_MSAL_REDIRECT_URI: z.string().nonempty(),
   CGN_IMAGE_BASE_URL: z.string().nonempty(),
-  CGN_RECAPTCHA_API_KEY: z.string().nonempty(),
   CGN_ALLOW_MULTIPLE_LOGIN: z.string().optional(),
   CGN_FRONTEND_URL: z.string().optional(),
   CGN_API_PROXY_TARGET_URL: z.string().optional()
