1ba37fff9463edbd72ee1dda55c5f3f7500500b5
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2026-05-21 11:20:20 +0200

    refactor: mui italia theme setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import "./utils/zod-csp-fix";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { registerLocale, setDefaultLocale } from "react-datepicker";
 import { it } from "date-fns/locale/it";
-import RouterConfig from "./navigation/RouterConfig";
-import "./styles/bootstrap-italia-fonts.scss";
-import "./styles/bootstrap-italia-custom.scss";
-import "./styles/react-datepicker-custom.scss";
-import "./styles/utils.scss";
+import { registerLocale, setDefaultLocale } from "react-datepicker";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import { queryClient } from "./api/common";
 import { AuthenticationProvider } from "./authentication/AuthenticationProvider";
 import { TooltipProvider } from "./context/TooltipProvider";
-import { renderCSP } from "./utils/meta";
+import RouterConfig from "./navigation/RouterConfig";
 import { store } from "./store/store";
+import "./styles/react-datepicker-custom.scss";
+import "./styles/utils.scss";
+import muiTheme from "./theme/muiTheme";
+import { renderCSP } from "./utils/meta";
+import "./utils/zod-csp-fix";
 
 renderCSP();
 
@@ -22,17 +23,20 @@ setDefaultLocale("it");
 
 function App() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <TooltipProvider>
-          <QueryClientProvider client={queryClient}>
-            <AuthenticationProvider>
-              <RouterConfig />
-            </AuthenticationProvider>
-          </QueryClientProvider>
-        </TooltipProvider>
-      </BrowserRouter>
-    </Provider>
+    <ThemeProvider theme={muiTheme}>
+      <CssBaseline />
+      <Provider store={store}>
+        <BrowserRouter>
+          <TooltipProvider>
+            <QueryClientProvider client={queryClient}>
+              <AuthenticationProvider>
+                <RouterConfig />
+              </AuthenticationProvider>
+            </QueryClientProvider>
+          </TooltipProvider>
+        </BrowserRouter>
+      </Provider>
+    </ThemeProvider>
   );
 }
 
